cd08b41202246a0263a5a30d04ed463feb2e3471
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-11-06 15:52:55 -0500

    working on create

--- practiceapp/forms.py
+++ practiceapp/forms.py
@@ -1,12 +1,12 @@
 #practiceapp/forms.py
 
 from django import forms
-from .models import Superhero
+from .models import SuperHero
 
 class SuperheroForm(forms.ModelForm):
 
     class Meta:
-        model=Superhero
+        model=SuperHero
         fields=('superhero_name','photo_url','real_name','place_of_birth','superpowers','occupation','group_affiliation','relatives','first_appear','publisher',)
 
 
