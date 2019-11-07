d7ca1a8f704d50e307eed2ddb277451e89bc0a53
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-11-07 11:10:24 -0500

    working on form

--- practice_django/settings.py
+++ practice_django/settings.py
@@ -40,9 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'practiceapp',
     'accounts',
+    'crispy_forms',
 ]
 
 LOGIN_REDIRECT_URL = '/' 
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
