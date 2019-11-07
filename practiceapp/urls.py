2d25a11eae70db7eca37d288e96d65d6234ddfd6
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-11-06 19:00:22 -0500

    working on update

--- practiceapp/urls.py
+++ practiceapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.superhero_list, name='superhero_list'),
-     path('heros/<int:pk>', views.superhero_detail, name='superhero_detail'),
-      path('heros/new', views.superhero_create, name='superhero_create'),
+    path('heros/<int:pk>', views.superhero_detail, name='superhero_detail'),
+    path('heros/new', views.superhero_create, name='superhero_create'),
+    path('heros/<int:pk>', views.superhero_edit, name='superhero_edit'),
 ]
--- practiceapp/views.py
+++ practiceapp/views.py
@@ -21,3 +21,16 @@ def superhero_create(request):
     else:
         form=SuperheroForm()
     return render(request, 'practiceapp/superhero_form.html',{'form':form})
+
+
+
+def superhero_edit(request,pk):
+    hero=SuperHero.objects.get(pk=pk)
+    if request.method=='POST':
+        form=SuperheroForm(request.POST,instance=hero)
+        if form.is_valid():
+            hero=form.save()
+            return redirect('superhero_detail', pk=hero.pk)
+    else:
+        form= SuperheroForm(instance=hero)
+    return render(request,'practiceapp/superhero_form.html',{'form':form})
