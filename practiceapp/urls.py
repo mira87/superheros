19a085a0c6d53634b7ac260cc0f83d967c331b71
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-11-06 20:04:08 -0500

    working on delete

--- practiceapp/urls.py
+++ practiceapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('heros/<int:pk>', views.superhero_detail, name='superhero_detail'),
     path('heros/new', views.superhero_create, name='superhero_create'),
     path('heros/<int:pk>', views.superhero_edit, name='superhero_edit'),
+    path('heros/<int:pk>/delete', views.superhero_delete, name='superhero_delete'),
 ]
