43b5d5182809dd569ab66b426f0c4fbd3a78ae79
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-11-06 22:26:18 -0500

    working on delete link

--- practiceapp/views.py
+++ practiceapp/views.py
@@ -30,7 +30,7 @@ def superhero_edit(request, pk):
         form = SuperheroForm(request.POST, instance=hero)
         if form.is_valid():
             hero = form.save()
-            return redirect('superhero_detail', pk=hero.pk)
+            return redirect('superhero_form', pk=hero.pk)
     else:
         form = SuperheroForm(instance=hero)
     return render(request, 'practiceapp/superhero_form.html', {'form': form})
