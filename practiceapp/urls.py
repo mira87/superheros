6735501662d1af6c5b21374ade2e056ad5a9c012
Shamira Jones <shamirajones@shamiras-mbp.home>
2019-11-07 00:22:39 -0500

    working on edit link

--- practiceapp/urls.py
+++ practiceapp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.superhero_list, name='superhero_list'),
     path('heros/<int:pk>', views.superhero_detail, name='superhero_detail'),
     path('heros/new', views.superhero_create, name='superhero_create'),
-    path('heros/<int:pk>', views.superhero_edit, name='superhero_edit'),
+    path('heros/<int:pk>/edit', views.superhero_edit, name='superhero_edit'),
     path('heros/<int:pk>/delete', views.superhero_delete, name='superhero_delete'),
 ]
